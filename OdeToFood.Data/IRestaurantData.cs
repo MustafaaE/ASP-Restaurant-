bf12c7128f2dd07edb739dd67ecb50a716a87861
Mustafa Elomeiri <Mustafaelomeiri@hotmail.se>
2022-07-01 15:19:49 +0200

    connect to local database and files needed for it

--- OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood.Data/IRestaurantData.cs
@@ -14,56 +14,8 @@ namespace OdeToFood.Data
         Restaurant Update(Restaurant updateRestaurant);
         //unit of work that collects changes and commits them 
         Restaurant Add(Restaurant newRestaurant);
+        Restaurant Delete(int id);
         int Commit();
-    }
-    public class InMemoryRestaurantData : IRestaurantData
-    {
-        readonly List<Restaurant> restaurants;
-        public InMemoryRestaurantData()
-        {
-            restaurants = new List<Restaurant>()
-            {
-                new Restaurant { Id = 1, Name = "Scott's Pizza", Location = "Lund", Cuisine = CuisineType.Mexican},
-                new Restaurant { Id = 2, Name = "Musti's Pizza", Location = "Malmö", Cuisine = CuisineType.None },
-                new Restaurant { Id = 3, Name = "Natalie's Pizza", Location = "Åstorp", Cuisine = CuisineType.Italian },
-
-            };
-        }
-
-        public Restaurant GetById(int id)
-        {
-            return restaurants.SingleOrDefault(r => r.Id == id);
-        }
-
-        public Restaurant Add(Restaurant newRestaurant)
-        {
-            restaurants.Add(newRestaurant);
-            newRestaurant.Id = restaurants.Max(r => r.Id) + 1;
-            return newRestaurant;
-        }
-        public Restaurant Update(Restaurant updatedRestaurant)
-        {
-            var restaurant = restaurants.Single(r => r.Id == updatedRestaurant.Id);
-            if (restaurant != null)
-            {
-                restaurant.Name = updatedRestaurant.Name;
-                restaurant.Location = updatedRestaurant.Location;
-                restaurant.Cuisine = updatedRestaurant.Cuisine;
-            }
-            return restaurant;
-        }
-
-        public int Commit()
-        {
-            return 0;
-        }
-        public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
-        {
-            return from r in restaurants
-                   where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
-                   orderby r.Name
-                   select r;
-        }
     }
 }
 
--- Pluralsight-course-ASP/Program.cs
+++ Pluralsight-course-ASP/Program.cs
@@ -1,11 +1,19 @@
+using Microsoft.EntityFrameworkCore;
 using OdeToFood.Data;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
-builder.Services.AddSingleton<IRestaurantData, InMemoryRestaurantData>();
-    
+builder.Services.AddScoped<IRestaurantData, SqlRestaurantData>();
+
+builder.Services.AddDbContext<OdeToFoodDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("OdeToFoodDb"));
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
