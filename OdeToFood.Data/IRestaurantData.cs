eef78c27af820d87a2db207ab4ef586271d94bb0
Mustafa Elomeiri <Mustafaelomeiri@hotmail.se>
2022-07-01 09:47:19 +0200

    Validation for edit page

--- OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood.Data/IRestaurantData.cs
@@ -11,6 +11,9 @@ namespace OdeToFood.Data
     {
         IEnumerable<Restaurant> GetRestaurantsByName(string name);
         Restaurant GetById(int id);
+        Restaurant Update(Restaurant updateRestaurant);
+        //unit of work that collects changes and commits them 
+        int Commit();
     }
     public class InMemoryRestaurantData : IRestaurantData
     {
@@ -20,7 +23,7 @@ namespace OdeToFood.Data
             restaurants = new List<Restaurant>()
             {
                 new Restaurant { Id = 1, Name = "Scott's Pizza", Location = "Lund", Cuisine = CuisineType.Mexican},
-                new Restaurant { Id = 2, Name = "Mustis Pizza", Location = "Malmö", Cuisine = CuisineType.None },
+                new Restaurant { Id = 2, Name = "Musti's Pizza", Location = "Malmö", Cuisine = CuisineType.None },
                 new Restaurant { Id = 3, Name = "Natalie's Pizza", Location = "Åstorp", Cuisine = CuisineType.Italian },
 
             };
@@ -29,6 +32,23 @@ namespace OdeToFood.Data
         public Restaurant GetById(int id)
         {
             return restaurants.SingleOrDefault(r => r.Id == id);
+        }
+
+        public Restaurant Update(Restaurant updatedRestaurant)
+        {
+            var restaurant = restaurants.Single(r => r.Id == updatedRestaurant.Id);
+            if (restaurant != null)
+            {
+                restaurant.Name = updatedRestaurant.Name;
+                restaurant.Location = updatedRestaurant.Location;
+                restaurant.Cuisine = updatedRestaurant.Cuisine;
+            }
+            return restaurant;
+        }
+
+        public int Commit()
+        {
+            return 0;
         }
         public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
         {
